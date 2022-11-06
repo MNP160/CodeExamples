4e029ded78c7739b3c41cffc2d5abf40a58b3eb1
mnp160 <mpenev13@gmail.com>
2022-11-06 21:58:19 +0200

    updating base model

--- src/main/java/com/example/CodeExamples/Models/Product.java
+++ src/main/java/com/example/CodeExamples/Models/Product.java
@@ -1,14 +1,21 @@
 package com.example.CodeExamples.Models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "products")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long productId;
-
 
     @Column(name = "name")
     private String name;
@@ -18,5 +25,7 @@ public class Product {
 
     @Column(name = "price")
     private Double price;
+
+
 
 }
